1478ec50da10d2aed797306cd41df5e4180efcbd
patrickofthenine <patrickaspeiser@gmail.com>
2019-05-08 20:06:53 -0400

    clean

--- oanda/price/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from pathlib import Path
-import os
-# Create your views here.
-
-def index(request):
-	resp = render(request, "price.html")
-	return resp
