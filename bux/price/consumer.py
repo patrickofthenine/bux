c5d1c97568f43081f67cc5832e779f24bace4882
patrickofthenine <patrickaspeiser@gmail.com>
2019-05-09 19:32:12 -0400

    getting accounts

--- bux/price/consumer.py
+++ bux/price/consumer.py
@@ -1,6 +1,9 @@
 import json
 import requests
 import yaml
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
 
 #	https://stream-fxpractice.oanda.com/
 class PriceConsumer():
@@ -8,6 +11,16 @@ class PriceConsumer():
 		return;	
 	def get_account_id(self):
 		with open('config.yml') as yams:
-			conf = yaml.safe_load(yams)
-			print(conf)
+			conf 				= yaml.safe_load(yams)
+			oanda_conf 			= conf['oanda']
+			oanda_api_config    = oanda_conf['api']
+			oanda_rest_base     = oanda_api_config['rest_base']
+			oanda_account_list  = oanda_api_config['paths']['rest']['account']['list']
+			token 				= oanda_conf['token']
+			req_url 			= oanda_rest_base + oanda_account_list
+			headers 			= {'Authorization': 'Bearer %s' %token}
+			print(headers)
+			res = requests.get(req_url, headers=headers)
+			pp.pprint(res.content)
+
 		return;	
